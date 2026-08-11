4170dc6569b7d30b4744eeb83650c4a07859a935
HereticJ <HereticJ@users.noreply.github.com>
2026-08-10 17:31:23 -0700

    Added CSS file, fixed errors from clicking menu button, added menu elements and text.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
-import { contentDiv, headline, desc } from "./homepage.js";
+import { headline, desc } from "./homepage.js";
+import { menuBtn } from "./menu.js";
 import mff from "./mff.jpg";
 
 const content = document.querySelector("#content");
-const image = document.createElement("img");
+export const image = document.createElement("img");
 image.src = mff;
 
 document.body.appendChild(image);
 
 const menu = document.querySelector("#menu");
-
-
-menu.addEventListener("click", checkMenu);
-    function checkMenu() {
-        content.removeChild(headline);
-        content.removeChild(desc);
-        document.body.removeChild(image);
-    };
+const about = document.querySelector("#about");
 
 console.log("Is this working yet?");
--- src/menu.js
+++ src/menu.js
@@ -1,23 +1,62 @@
-export { menuHeadline, beverages, sides, meals, desserts }
+import { headline, desc } from "./homepage.js";
+import { image } from "./index.js";
+export { menuBtn, menuHeadline, beverages, sides, meals, desserts };
 
 const menuHeadline = document.createElement("h1");
-const meals = document.createElement("h3");
-const familyFeedbag = document.createElement("h6");
-const chickenFS = document.createElement("h6");
-const burger = document.createElement("h6");
-const clubSW = document.createElement("h6");
-const beverages = document.createElement("h4");
-const sides = document.createElement("h4");
-const desserts = document.createElement("h4");
+const meals = document.createElement("h2");
+const beverages = document.createElement("h2");
+const sides = document.createElement("h2");
+const desserts = document.createElement("h2");
+const chickenFS = document.createElement("h4");
+const burger = document.createElement("h4");
+const clubSW = document.createElement("h4");
+const beer1 = document.createElement("h4");
+const beer2 = document.createElement("h4");
+const wine1 = document.createElement("h4");
+const wine2 = document.createElement("h4");
+const cola1 = document.createElement("h4");
+const cola2 = document.createElement("h4");
 let description = document.createElement("p");
 
 
 menuHeadline.textContent = "Menu";
 meals.textContent = "Main Courses";
-beverages.textContent = "Refreshments";
+beverages.textContent = "Drinks";
 sides.textContent = "Sides";
 desserts.textContent = "Treats";
 
+const menuBtn = document.querySelector("#menu");
+menuBtn.addEventListener("click", menuPage);
+    function menuPage() {
+        content.removeChild(headline);
+        content.removeChild(desc);
+        document.body.removeChild(image);
+        content.appendChild(menuHeadline);
+        menuHeadline.textContent = "Feedbag Favorites";
+            content.appendChild(meals);
+                meals.appendChild(chickenFS);
+                meals.appendChild(burger);
+                meals.appendChild(clubSW);
+                    chickenFS.textContent = "Chicken Fried Steak";
+                    burger.textContent = "Moe's Patty";
+                    clubSW.textContent = "Club Sandwich";
+            content.appendChild(beverages);
+                beverages.appendChild(beer1);
+                beverages.appendChild(beer2);
+                beverages.appendChild(wine1);
+                beverages.appendChild(wine2);
+                beverages.appendChild(cola1);
+                beverages.appendChild(cola1);
+                    beer1.textContent = "Duff";
+                    beer2.textContent = "Duff Lite";
+                    wine1.textContent = "White Zinfandel";
+                    wine2.textContent = "Red Zinfandel";
+                    cola1.textContent = "Buzz Cola";
+                    cola2.textContent = "Diet Buzz Cola";
+        
+        content.appendChild(sides);
+        content.appendChild(desserts);
+    };
 
 
 
