a89f9bf9c5b325dfdca6273f789fe69d1e5000a7
HereticJ <HereticJ@users.noreply.github.com>
2026-08-18 17:30:26 -0700

    Fixed issue with menu page loading below homepage, reworked main dishes for menu to reflect actual episode, added donuts to desserts menu.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
-import { headline, desc, content, image } from "./homepage.js";
-import { menuFill } from "./menu.js";
 import "./styles.css";
+import { headline, desc, content, image } from "./homepage.js";
+import { meals, mealsTitle, hotDog, deepFried, fish, drinks, drinksTitle,
+     beer1, beer2, wine1, wine2, cola1, cola2, sides, sidesTitle, chips,
+     fries, chili, wings, pizza, spaghetti, desserts, dessertsTitle } 
+     from "./menu.js";
 
 // Initializes tab buttons.
 const homeBtn = document.querySelector("#home");
 const menuBtn = document.querySelector("#menu");
 const aboutBtn = document.querySelector("#about");
-
-// Returns homepage when clicked.
-homeBtn.addEventListener("click", homePage);
-function homePage() {
-    removeTab();
-    content.appendChild(headline);
-    content.appendChild(image);
-    content.appendChild(desc);
-};
 
 // Removes homepage elements from page and switches to menu tab.
 function removeTab() {
@@ -24,18 +18,67 @@ function removeTab() {
         };
 };
 
+// Returns homepage when clicked.
+homeBtn.addEventListener("click", homePage);
+    function homePage() {
+        removeTab();
+        content.appendChild(headline);
+        content.appendChild(image);
+        content.appendChild(desc);
+    };
+
 // Switches to Menu tab when clicked.
 menuBtn.addEventListener("click", menuPage);
     function menuPage() {
         removeTab();
-        menuFill;
+        menuContents();
     };
 
 // Switches to About tab when clicked.
-menuBtn.addEventListener("click", aboutPage);
+aboutBtn.addEventListener("click", aboutPage);
     function aboutPage() {
         removeTab();
-
     };
+
+// Removes content from current tab and loads in Homepage content.
+function homeContents() {
+    content.appendChild(headline);
+    content.appendChild(image);
+    content.appendChild(desc);
+}
+
+// Fills menu tab with menu content.
+function menuContents() {
+    // Adds menu title and lists meals.
+    meals.appendChild(mealsTitle);
+        meals.appendChild(hotDog);
+        meals.appendChild(deepFried);
+        meals.appendChild(fish);
+            content.appendChild(meals);
+
+    // Appends drinks to DOM.
+    drinks.appendChild(drinksTitle);
+        content.appendChild(drinks);
+            drinks.appendChild(beer1);
+            drinks.appendChild(beer2);
+            drinks.appendChild(wine1);
+            drinks.appendChild(wine2);
+            drinks.appendChild(cola1);
+            drinks.appendChild(cola1);
+
+    // Appends sides to DOM.
+    sides.appendChild(sidesTitle);
+        content.appendChild(sides);
+            sides.appendChild(chips);
+            sides.appendChild(fries);
+            sides.appendChild(chili);
+            sides.appendChild(wings);
+            sides.appendChild(pizza);
+            sides.appendChild(spaghetti);
+        
+    // Appends desserts to DOM.
+    desserts.appendChild(dessertsTitle);
+        content.appendChild(desserts);
+};
 
 console.log("Is this working yet?");
--- src/menu.js
+++ src/menu.js
@@ -1,30 +1,44 @@
-import { content } from "./homepage.js";
-export { menuFill };
-
 // Describes menu items.
 let itemDesc = document.createElement("p");
 
+// Sides title, list of h3 elements for each entry, adds p descriptions.
+export const sides = document.createElement("div#sides");
+    export const sidesTitle = document.createElement("h3");
+        sidesTitle.textContent = "Sides";
+            export const chips = document.createElement("h4");
+            export const fries = document.createElement("h4");
+            export const chili = document.createElement("h4");
+            export const wings = document.createElement("h4");
+            export const pizza = document.createElement("h4");
+            export const spaghetti = document.createElement("h4");
+                chips.textContent = "Fiestaritos";
+                fries.textContent = "Million Dollar Birthday Fries";
+                chili.textContent = "Moe's Hobo Chicken Chili";
+                wings.textContent = "Ranchy Wingy Thingys";
+                pizza.textContent = "Southwestern Pizza Fingers";
+                spaghetti.textContent = "Spaghetti and Moe Balls";
+
 // Meals title, list of h3 elements for each entry, adds p descriptions.
-const meals = document.createElement("div#meals");
-    const mealsTitle = document.createElement("h3");
+export const meals = document.createElement("div#meals");
+    export const mealsTitle = document.createElement("h2");
         mealsTitle.textContent = "Main Courses";
-            const chickenFS = document.createElement("h5");
-            const burger = document.createElement("h5");
-            const clubSW = document.createElement("h5");
-                chickenFS.textContent = "Chicken Fried Steak";
-                burger.textContent = "Patty 'n Selma";
-                clubSW.textContent = "Club Sandwich";
+            export const hotDog = document.createElement("h3");
+            export const deepFried = document.createElement("h3");
+            export const fish = document.createElement("h3");
+                hotDog.textContent = "Amtrak-style Hot Dog";
+                deepFried.textContent = "Deep-fried home-style meals";
+                fish.textContent = "Guilt-Free Steakfish Filets";
 
 // Drinks title, list of h3 elements for each entry, adds p descriptions.
-const drinks = document.createElement("div#drinks");
-    const drinksTitle = document.createElement("h3");
+export const drinks = document.createElement("div#drinks");
+    export const drinksTitle = document.createElement("h2");
         drinksTitle.textContent = "Drinks";
-            const beer1 = document.createElement("h5");
-            const beer2 = document.createElement("h5");
-            const wine1 = document.createElement("h5");
-            const wine2 = document.createElement("h5");
-            const cola1 = document.createElement("h5");
-            const cola2 = document.createElement("h5");
+            export const beer1 = document.createElement("h4");
+            export const beer2 = document.createElement("h4");
+            export const wine1 = document.createElement("h4");
+            export const wine2 = document.createElement("h4");
+            export const cola1 = document.createElement("h4");
+            export const cola2 = document.createElement("h4");
                 beer1.textContent = "Duff";
                 beer2.textContent = "Duff Lite";
                 wine1.textContent = "White Zinfandel";
@@ -32,45 +46,12 @@ const drinks = document.createElement("div#drinks");
                 cola1.textContent = "Buzz Cola";
                 cola2.textContent = "Diet Buzz Cola";
 
-// Siodes title, list of h3 elements for each entry, adds p descriptions.
-const sides = document.createElement("div#sides");
-    const sidesTitle = document.createElement("h3");
-        sidesTitle.textContent = "Sides";
-
 // Desserts title, list of h3 elements for each entry, adds p descriptions.
-const desserts = document.createElement("div#desserts");
-    const dessertsTitle = document.createElement("h3");
+export const desserts = document.createElement("div#desserts");
+    export const dessertsTitle = document.createElement("h3");
         dessertsTitle.textContent = "Desserts";
+            export const donuts = document.createElement("h4");
 
-// Fills menu tab with menu content.
-function menuContents() {
-    // Adds menu title and lists meals.
-    meals.appendChild(mealsTitle);
-        meals.appendChild(chickenFS);
-        meals.appendChild(burger);
-        meals.appendChild(clubSW);
-            content.appendChild(meals);
-
-    // Appends drinks to DOM.
-    drinks.appendChild(drinksTitle);
-        content.appendChild(drinks);
-            drinks.appendChild(beer1);
-            drinks.appendChild(beer2);
-            drinks.appendChild(wine1);
-            drinks.appendChild(wine2);
-            drinks.appendChild(cola1);
-            drinks.appendChild(cola1);
-
-    // Appends sides to DOM.
-    sides.appendChild(sidesTitle);
-        content.appendChild(sides);
-        
-    // Appends desserts to DOM.
-    desserts.appendChild(dessertsTitle);
-        content.appendChild(desserts);
-};
-
-const menuFill = menuContents();
 
 
 
